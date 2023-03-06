1dc524d200c861ea749365df50287ddcc62f0ad2
Paulette Mejia <124909264+paulettemejiar@users.noreply.github.com>
2023-03-06 07:41:22 +0000

    this the one

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ class Main {
       } else if (input == 5) {
         break;
       } else if (input == 4) {
-          System.out.println("Cuurent Students:");
+          System.out.println("Current Students:");
            while(fileScanner.hasNextLine()){
              
         System.out.println(fileScanner.nextLine());
